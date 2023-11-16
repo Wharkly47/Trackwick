af8ae222514e16fe6ec9eebede6dfabfcc88b8cd
Wharkly-47 <130136825+Wharkly47@users.noreply.github.com>
2023-11-17 02:43:42 +0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -18,3 +18,19 @@ app.post('/enregistrer-clic', (req, res) => {
 app.listen(3000, () => {
   console.log('Serveur démarré sur le port 3000');
 });
+
+{
+  "name": "nom-de-votre-projet",
+  "version": "1.0.0",
+  "description": "Description de votre projet",
+  "main": "app.js",
+  "dependencies": {
+    "express": "^4.17.1",
+    "body-parser": "^1.19.0" // Ajout de body-parser pour analyser le corps des requêtes
+  },
+  "scripts": {
+    "start": "node app.js"
+  },
+  "author": "Wharkly47",
+  "license": "Licence souhaitée (par exemple MIT)"
+}
